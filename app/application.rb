5d29eaa6c99ea09b29e8145df135b93f6c2da6fa
Christopher Beltran <christod.beltran@gmail.com>
2018-11-06 04:58:49 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,8 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-    @@items
+    @@cart.each do |item|
+        resp.write "#{item}\n"
     elsif req.path 
     else
       resp.write "Path Not Found"
